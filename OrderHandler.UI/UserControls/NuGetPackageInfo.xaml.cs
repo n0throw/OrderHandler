e392d351329bc6db4f1b74f011b1023d34442980
n0throw <yaroslaw.yaroslaw32@gmail.com>
2023-05-30 17:12:50 +0500

    Добавил окно с лицензиями

---
+++ OrderHandler.UI/UserControls/NuGetPackageInfo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OrderHandler.UI.UserControls; 
+
+public partial class NuGetPackageInfo : UserControl {
+	public NuGetPackageInfo() {
+		InitializeComponent();
+		DataContext = this;
+	}
+	
+	public Uri Link { get; set; }
+	public string PackageName { get; set; }
+	public string PackageVersion { get; set; }
+
+	void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+		=> Process.Start(Link.ToString());
+}
---
+++ OrderHandler.UI/Windows/UserAgreement.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace OrderHandler.UI.Windows; 
+
+public partial class UserAgreement : Window {
+	public UserAgreement() {
+		InitializeComponent();
+	}
+}
